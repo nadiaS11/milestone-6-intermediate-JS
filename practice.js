9e43484732c22a26d02998afaf40de22ecbbd044
nadiaS11 <nadiasultana.ns11@gmail.com>
2023-08-25 10:35:18 +0600

    file for module 31 practice

--- practice.js
+++ practice.js
@@ -54,3 +54,8 @@ console.log(y);
 const nums = [1,2,3,4,5];
 let output = nums.filter(n => n%2);
 console.log(output);
+const sum =(p, q)=> {
+  p + q;
+ }
+ const result = sum(2, 3);
+ console.log(result);
